b2998c117cd205fbe27da2050f81f1de2795b2ff
martin.mitev <martin.mitev@sciant.com>
2025-07-14 17:44:31 +0300

    Configured socket IO connection

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,10 @@
 'use client'
 
-import { useSocket } from '@/websocket/socketProvider.js';
-import { useEffect } from 'react';
 import InnerPageContainer from '@/ui/components/innerPageContainer.js';
 import { Title } from '@mantine/core';
 import ChatTypesContainer from '@/ui/components/home/chatTypesContainer.js';
 
 export default function Home() {
-  const socket = useSocket(); 
-  useEffect(() => {
-    if (!socket) return;
-    socket.on("sendMessage", (message) => {
-      console.log(message); 
-	});
-	return () => {
-		socket.off("sendMessage");
-	};
-}, [socket]);
-socket.emit('sendMessage', "Hello World");
   return (
     <InnerPageContainer>
       <Title mb="lg" order={2} textWrap="wrap">
--- websocket/app.js
+++ websocket/app.js
@@ -1,18 +1,26 @@
+import express from 'express';
+import { createServer } from 'node:http';
 import { Server } from "socket.io";
 import socketHandler from './sockets/index.js';
 
+const app = express();
 const PORT = 3005;
-const io = new Server({
+const server = createServer(app);
+const io = new Server(server, {
     cors: {
-        origin: "*",  
+        origin: "*",
         methods: ["GET", "POST"]
     }
+});
+
+app.get('/', (req, res) => {
+    res.send({"status": 200});
 });
 
 io.on('connection', (socket) => {
     socketHandler(socket);
 });
 
-io.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
